5c9a4294459e89513fd4df5f8ff77e202ee75cb2
Timofey <timofeyvalerievich@gmail.com>
2023-09-19 23:31:55 +0300

    routing HandleFunc

---
+++ routing/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/hello/", hello)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Home"))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	name := strings.Split(r.URL.Path, "/")[2]
+	w.Write([]byte(fmt.Sprintf("Hello, %s", name)))
+	return
+}
