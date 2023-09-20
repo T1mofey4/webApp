f3cb32e163b67d14b369487deb84905afb00e865
Timofey <timofeyvalerievich@gmail.com>
2023-09-21 00:06:01 +0300

    json answer

--- structToJSON/main.go
+++ structToJSON/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"net/http"
 )
 
 type User struct {
@@ -11,9 +11,23 @@ type User struct {
 }
 
 func main() {
+	// user1 := User{Name: "Ivan", Id: 222}
+	// bytes, _ := json.Marshal(user1)
+	// bytes1, _ := json.MarshalIndent(user1, "", "  ") //second way with Tab
+	// fmt.Println(string(bytes))
+	// fmt.Println(string(bytes1))
+
+	http.HandleFunc("/user", UserHandler)
+	http.ListenAndServe(":3000", nil)
+}
+
+func UserHandler(w http.ResponseWriter, r *http.Request) {
 	user1 := User{Name: "Ivan", Id: 222}
-	bytes, _ := json.Marshal(user1)
-	bytes1, _ := json.MarshalIndent(user1, "", "  ") //second way with Tab
-	fmt.Println(string(bytes))
-	fmt.Println(string(bytes1))
+	bytes, err := json.Marshal(user1)
+	if err != nil {
+		w.Write([]byte("500 Internal Server Error" + err.Error()))
+		return
+	}
+	w.Header().Set("ContentType", "application/json")
+	w.Write(bytes)
 }
