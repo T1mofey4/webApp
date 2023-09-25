374de343594e86ff486674f9aa60ff6779c36a0b
T1mofey4 <timofeyvalerievich@gmail.com>
2023-09-25 18:08:31 +0300

    get data from form

---
+++ getDataFromForm/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/form", FormHandler)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func FormHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		io.WriteString(w, "405 Method Not Allowed")
+		return
+	}
+
+	io.WriteString(w, "OK")
+}
