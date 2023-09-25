f06cbeffb41c219928b90ad60a3539699530f661
Timofey <timofeyvalerievich@gmail.com>
2023-09-26 00:42:32 +0300

    downloading files from form

---
+++ downloadFiles/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+)
+
+func main() {
+	http.HandleFunc("/form", FormHandler)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func FormHandler(w http.ResponseWriter, r *http.Request) {
+	os.Mkdir("./tmp", os.FileMode(0522))
+	err := r.ParseMultipartForm(10 * 1024 * 1024)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, err.Error())
+		return
+	}
+
+	file, header, err := r.FormFile("myfile")
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, err.Error())
+		return
+	}
+	defer file.Close()
+	fmt.Println("filename", header.Filename)
+	fmt.Println("MIME Type", header.Header)
+	fmt.Println("Size", header.Size)
+
+	ext := path.Ext(header.Filename)
+	tempFile, err := ioutil.TempFile("./tmp", "*"+ext)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, err.Error())
+		return
+	}
+	defer tempFile.Close()
+
+	fmt.Println("save to ", tempFile.Name())
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, err.Error())
+		return
+	}
+
+	_, err = tempFile.Write(bytes)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, err.Error())
+		return
+	}
+
+	fmt.Fprintf(w, "File uploaded!")
+}
